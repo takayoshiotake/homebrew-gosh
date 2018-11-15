088f9c6078a9f218a54071ec660f913bf33c68b1
OTAKE Takayoshi <takayoshiotake@users.noreply.github.com>
2018-11-15 12:34:28 +0900

    Update and rename fnameext.rb to nameext.rb

--- Formula/nameext.rb
+++ Formula/nameext.rb
@@ -1,15 +1,15 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://www.rubydoc.info/github/Homebrew/brew/master/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-class Fnameext < Formula
+class Nameext < Formula
   desc ""
   homepage ""
-  url "https://github.com/takayoshiotake/fnameext/archive/v1.0.0.tar.gz"
-  sha256 "e9a4685d5ea399ebb397cda414e42fbca7588bd203f97484059d251348596ef8"
+  url "https://github.com/takayoshiotake/nameext/archive/v1.0.0.tar.gz"
+  sha256 "c9632d1e450e1ca0f854cdda693ef15d76bc58882809ddc20bee80287a304dce"
   # depends_on "cmake" => :build
 
   def install
-    bin.install "fnameext"
+    bin.install "nameext"
   end
 
   test do
@@ -17,7 +17,7 @@ class Fnameext < Formula
     #
     # This test will fail and we won't accept that! For Homebrew/homebrew-core
     # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test fnameext`. Options passed
+    # software. Run the test with `brew test nameext`. Options passed
     # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
     #
     # The installed folder is not in the path, so use the entire path to any
